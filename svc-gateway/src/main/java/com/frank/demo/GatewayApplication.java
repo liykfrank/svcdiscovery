0e8f00924c5bfbeaaf23b6f63827153a9bf173d2
liykfrank <liyk.frank.public@gmail.com>
2018-07-27 02:43:35 -0400

    gateeway filter and authentication

--- svc-gateway/src/main/java/com/frank/demo/GatewayApplication.java
+++ svc-gateway/src/main/java/com/frank/demo/GatewayApplication.java
@@ -9,19 +9,23 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.ReactiveAuthorizationManager;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -53,12 +57,49 @@ public class GatewayApplication {
                         }
                         )).uri("lb://svc-agency-service"))
                 .route("greeting", r -> r.path("/gre/**")
+                		.filters(f -> f.rewritePath("/gre/(?<segment>.*)",
+								"/${segment}"))
+//                		.filters(f -> f.setPath("/"))
+//                		.filters(f -> f.rewritePath("/gre/(?<segment>.*)",
+//								"/${segment}"))
                         .uri("lb://svc-risk-service"))
 //				.route("test", r -> r.path("/**")
 //						.uri("lb://svc-agency-service"))
 				.build();
 	}
 
+	@Bean
+	SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
+		return http.httpBasic().and()
+				.csrf().disable()
+				.authorizeExchange()
+				.pathMatchers("/agent/**").authenticated()
+				
+//				
+//				.pathMatchers("/users/{userName}").hasRole("ADMIN")
+//				.access(new ReactiveAuthorizationManager<AuthorizationContext>() {
+//					@Override
+//					public Mono<AuthorizationDecision> check(Mono<Authentication> mono,
+//							AuthorizationContext context) {
+//						String uname = (String) context.getVariables().get("userName");
+////						return null;
+//						return mono.map(auth -> auth.getName().equals(uname))
+//								.map(AuthorizationDecision::new);
+//
+//					}
+//				})
+				
+				.anyExchange().permitAll()
+				.and()
+				.build();
+	}
+
+	@Bean
+	public MapReactiveUserDetailsService reactiveUserDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("frank").password("frank").roles("USER").build();
+		return new MapReactiveUserDetailsService(user);
+	}
+	
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,
